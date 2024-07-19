c601e6fb28fb237173811c35035a157ed06f18f5
Iván Ruiz <ivr.644@gmail.com>
2024-07-20 00:11:21 +0200

    Added mdxeditor

--- src/pods/content/content.container.tsx
+++ src/pods/content/content.container.tsx
@@ -1,15 +1,43 @@
+import {
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  BoldItalicUnderlineToggles,
+  UndoRedo,
+  toolbarPlugin,
+} from "@mdxeditor/editor";
+import "@mdxeditor/editor/style.css";
 import { useNotesContext } from "@/core/providers/notes/notes.provider";
 
 export const Content = () => {
   const { notes, activeNoteId } = useNotesContext();
   const activeNote = notes.find((note) => note.id === activeNoteId);
-
   return (
     <div className="w-3/4 p-4">
       {activeNote ? (
         <>
-          <h2 className="mb-4 text-2xl">{activeNote.name}</h2>
-          <p>{activeNote.content}</p>
+          <MDXEditor
+            markdown={activeNote.content}
+            plugins={[
+              headingsPlugin(),
+              listsPlugin(),
+              quotePlugin(),
+              thematicBreakPlugin(),
+              markdownShortcutPlugin(),
+              toolbarPlugin({
+                toolbarContents: () => (
+                  <>
+                    {" "}
+                    <UndoRedo />
+                    <BoldItalicUnderlineToggles />
+                  </>
+                ),
+              }),
+            ]}
+          />
         </>
       ) : (
         <p>No note selected</p>
