5dc17c8f34c327f8bde8910325a6716153254190
Iván Ruiz <ivr.644@gmail.com>
2024-07-22 22:25:12 +0200

    Added folder collapse

--- src/pods/aside/components/folder.component.tsx
+++ src/pods/aside/components/folder.component.tsx
@@ -3,6 +3,7 @@ import { NoteItem } from "./note.component";
 import { NoteModel } from "@/core/providers";
 import { dropTargetForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import invariant from "tiny-invariant";
+import { ChevronDown, ChevronRight } from "@/common/components";
 
 interface FolderProps {
   folder: {
@@ -33,6 +34,7 @@ export const Folder: React.FC<FolderProps> = ({
 }) => {
   const ref = useRef(null);
   const [isDraggedOver, setIsDraggedOver] = useState(false);
+  const [isFolderCollapsed, setFolderCollapsed] = useState(false);
 
   useEffect(() => {
     const el = ref.current;
@@ -49,7 +51,7 @@ export const Folder: React.FC<FolderProps> = ({
   return (
     <div
       key={folder.id}
-      className="mb-4"
+      className="flex flex-col gap-4"
       ref={ref}
       style={
         isDraggedOver
@@ -58,22 +60,37 @@ export const Folder: React.FC<FolderProps> = ({
       }
     >
       {activeWorkspaceId !== folder.id && (
-        <h3 className="text-lg font-semibold">{folder.name}</h3>
+        <div className="flex gap-3">
+          {isFolderCollapsed ? <ChevronRight /> : <ChevronDown />}
+          <h3
+            onClick={() =>
+              isFolderCollapsed
+                ? setFolderCollapsed(false)
+                : setFolderCollapsed(true)
+            }
+            className="font-semibold cursor-pointer select-none"
+          >
+            {folder.name}
+          </h3>
+        </div>
       )}
-      {folder.notes.map((note) => (
-        <NoteItem
-          key={note.id}
-          note={note}
-          editingNoteId={editingNoteId}
-          newNoteName={newNoteName}
-          folder={folder}
-          onClick={() => setActiveNoteId(note.id)}
-          onContextMenu={(e) => handleContextMenu(e, note)}
-          onInputChange={handleInputChange}
-          onInputBlur={handleInputBlur}
-          onKeyDown={handleKeyDown}
-        />
-      ))}
+      {folder.notes.map(
+        (note) =>
+          !isFolderCollapsed && (
+            <NoteItem
+              key={note.id}
+              note={note}
+              editingNoteId={editingNoteId}
+              newNoteName={newNoteName}
+              folder={folder}
+              onClick={() => setActiveNoteId(note.id)}
+              onContextMenu={(e) => handleContextMenu(e, note)}
+              onInputChange={handleInputChange}
+              onInputBlur={handleInputBlur}
+              onKeyDown={handleKeyDown}
+            />
+          )
+      )}
     </div>
   );
 };
